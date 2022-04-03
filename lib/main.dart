069008bb903a01023ed1811b0e5fa2391d661bc3
BaselAlaa22 <baselalaagit@gmail.com>
2022-04-03 16:17:52 +0200

    Flutter xylophone project

---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:audioplayers/audioplayers.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(XylophoneApp());
+
+class XylophoneApp extends StatelessWidget {
+  void playSound(int noteNumber) {
+    final player = AudioCache();
+    player.play('note$noteNumber.wav');
+  }
+
+  Expanded buildKey(int noteNumber, Color color) {
+    return Expanded(
+      child: MaterialButton(
+        onPressed: () {
+          playSound(noteNumber);
+        },
+        child: Text(""),
+        color: color,
+        enableFeedback: false,
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              buildKey(1, Colors.red),
+              buildKey(2, Colors.orange),
+              buildKey(3, Colors.yellow),
+              buildKey(4, Colors.green),
+              buildKey(5, Colors.teal),
+              buildKey(6, Colors.blue),
+              buildKey(7, Colors.deepPurple),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
